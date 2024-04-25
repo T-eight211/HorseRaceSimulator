5fc79f02b65217fe1dcb1551b0edc14ef1727311
Taher Ahmed <taherahmed@Tahers-MacBook-Air.local>
2024-04-26 00:13:27 +0100

    Final-3

--- Part2/src/Game.java
+++ Part2/src/Game.java
@@ -3,13 +3,17 @@ import java.awt.*;
 public class Game {
 
    public static JFrame frame;
-   
-    public static void main(String[] args) {
+
+   public static void startRaceGUI (){
       frame = new JFrame("Horse Game");
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       GameWorld gameWorld = new GameWorld();
       gameWorld.addComponents(frame);
       frame.add(Background.panel, BorderLayout.CENTER);
       frame.setVisible(true);
+   }
+   
+    public static void main(String[] args) {
+   startRaceGUI();
     }
 }
